4798bac8de47b6866afc377839ca3a258c63be2e
scarytom <t.denley@cantab.net>
2011-06-12 13:37:43 +0100

    a simple http server is born

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-/**
- * 
- */
-console.log("Hello, world");
+"user strict";
+
+var http = require("http"),
+    server;
+
+server = http.createServer(function(request, response) {
+	response.writeHeader(200, {
+		"content-type": "text/plain"
+	});
+	response.write("Hello, world");
+	response.end();
+});
+
+server.listen("8888");
