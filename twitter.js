d2bb7af4608b5c9c8a929e9562dbaa122c1036f4
scarytom <t.denley@cantab.net>
2011-06-12 15:54:18 +0100

    twitter module

--- twitter.js
+++ twitter.js
@@ -1,17 +1,19 @@
 (function() {
 	"user strict";
-	var http = require("http"),
-	    options = {
-	        host: 'search.twitter.com',
-	        port: 80,
-	        path: '/search.json?q=spa2011'
-	    };
+	var http = require("http");
+	    
 	
-	module.exports.get = function(handler) {
+	module.exports.search = function(queryString, handler) {
+		var options = {
+		        host: "search.twitter.com",
+		        port: 80,
+		        path: "/search.json?q=" + queryString
+		    };
+		
 		http.get(options, function(res) {
 		    console.log("Got response: " + res.statusCode);
-	        res.on('data', function(d) {
-	            handler(d);
+	        res.on('data', function(data) {
+	            handler(data);
 	        });
 	    }).on('error', function(e) {
 	        console.log("Got error: " + e.message);
