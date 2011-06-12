7d392dd66e06e45936a27252922a5e86e16d0a18
scarytom <t.denley@cantab.net>
2011-06-12 14:47:41 +0100

    make a simple calculator

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 "user strict";
 
 var http = require("http"),
-    server;
+    server,
+    accumulator = 0;
 
 server = http.createServer(function(request, response) {
+	var value, operation, path;
 	response.writeHeader(200, {
 		"content-type": "text/plain"
 	});
-	response.write("Hello, world");
+	
+	path = request.url.split("/");
+	operation = path[1];
+	value = parseInt(path[path.length - 1], 10);
+	
+	if (value || value === 0) {
+		if (operation === "multiply") {
+			accumulator *= value;
+		}
+		else if (operation === "divide") {
+			accumulator /= value;
+		}
+		else if (operation === "subtract") {
+			accumulator -= value;
+		}
+		else {
+			accumulator += value;
+		}
+	}
+	response.write("" + accumulator);
 	response.end();
 });
 
